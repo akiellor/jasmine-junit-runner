3a3b59fd8e36ca152d4080ee8d3e1ef882489f7b
Andrew Kiellor <akiellor@gmail.com>
2013-03-07 22:24:21 -0800

    Killed the bin directory.

--- bin/bootstrap.js
+++
@@ -1,9 +0,0 @@
-load("./../src/test/javascript/lib/env.rhino.1.2.js");
-load("./../src/test/javascript/lib/env.utils.js");
-load("./../src/test/javascript/envJsOptions.js");
-
-load("./../src/test/javascript/jquery-1.6.1.js");
-
-function append(itm) {
-	$('body').append($(itm));
-}
--- bin/rhino.debug.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -cp js.jar org.mozilla.javascript.tools.debugger.Main -opt -1
