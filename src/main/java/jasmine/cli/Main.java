3c8e33dc0c950d9967c95893d1d932f0f7c14311
Andrew Kiellor <akiellor@gmail.com>
2013-07-03 09:02:30 -0700

    Better error reporting for cli.

--- src/main/java/jasmine/cli/Main.java
+++ src/main/java/jasmine/cli/Main.java
@@ -1,11 +1,15 @@
 package jasmine.cli;
 
+import com.google.common.collect.Lists;
 import jasmine.runtime.Configuration;
 import jasmine.runtime.Hooks;
 import jasmine.runtime.It;
 import jasmine.runtime.Notifier;
 import jasmine.runtime.rhino.RhinoBackend;
 import jasmine.runtime.rhino.RhinoContext;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String... args) {
@@ -33,18 +37,49 @@ public class Main {
     }
 
     private static class CliNotifier implements Notifier {
+        private static class Failure{
+            private final Throwable error;
+            private final It it;
+
+            public Failure(It it, Throwable error){
+                this.it = it;
+                this.error = error;
+            }
+
+            public String format() {
+                StringBuilder builder = new StringBuilder()
+                    .append(it.getId())
+                    .append(" - ")
+                    .append(it.getDescription())
+                    .append("\n\n");
+
+
+                builder.append(error.getMessage())
+                        .append("\n\n");
+
+                return builder.toString();
+            }
+        }
+
+        List<Failure> failures = new CopyOnWriteArrayList<Failure>();
+        boolean testRun = false;
+
         @Override
         public void pass(It it) {
             System.out.print(".");
+            testRun = true;
         }
 
         @Override
         public void fail(It it, Throwable error) {
             System.out.print("F");
+            testRun = true;
+            failures.add(new Failure(it, error));
         }
 
         @Override
         public void skipped(It it) {
+            testRun = true;
             System.out.print("-");
         }
 
@@ -55,6 +90,14 @@ public class Main {
 
         @Override
         public void finished() {
+            if(testRun){
+                System.out.println();
+                System.out.println();
+
+                for(Failure failure : failures){
+                    System.out.println(failure.format());
+                }
+            }
         }
     }
 }
